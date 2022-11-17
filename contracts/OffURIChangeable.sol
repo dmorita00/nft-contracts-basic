59f91c5f8b5717e9cd1afd0781b759c7cb389587
dmorita00 <ingressmrt@gmail.com>
2022-11-18 00:23:25 +0900

    OffURIChangeable

---
+++ contracts/OffURIChangeable.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.14;
+
+import "@openzeppelin/contracts@4.6.0/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@4.6.0/access/Ownable.sol";
+import "@openzeppelin/contracts@4.6.0/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract OffURIChangeable is ERC721URIStorage, Ownable {
+    constructor() ERC721("OffURIChangeable", "OffURIChangeable") {}
+
+    function nftMint(uint256 tokenId) public onlyOwner {
+        _mint(_msgSender(), tokenId);
+    }
+
+    function setTokenURI(uint256 tokenId, string calldata uri) public onlyOwner {
+        _setTokenURI(tokenId, uri);
+    }
+}
